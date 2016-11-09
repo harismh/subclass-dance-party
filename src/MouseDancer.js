171cf804945fdd691f22b9c95ccf98f67d7eeaf4
harismh <harismh@icloud.com>
2016-11-08 16:06:01 -0800

    added CSS, music, and backgrounds; pair Don and Haris

--- src/MouseDancer.js
+++ src/MouseDancer.js
@@ -13,3 +13,35 @@ MouseDancer.prototype.followMouse = function(x, y) {
 
   this.$node.css(styleSettings);
 };
+
+MouseDancer.prototype.getPos = function(node) {
+  var pos = node.$node.position();
+  return pos; 
+};
+
+MouseDancer.prototype.setPos = function(node) {
+  var dancerpos = this.getPos(this);
+  var nodepos = this.getPos(node);
+
+  var dancertop = dancerpos.top;
+  var dancerleft = dancerpos.left;
+  var nodetop = nodepos.top;
+  var nodeleft = nodepos.left;
+
+  if (dancerleft - 50 > nodeleft) {
+    node.$node.css('left', nodeleft += 3);
+  } else if (dancerleft + 50 < nodeleft) {
+    node.$node.css('left', nodeleft -= 3);    
+  } else if (dancertop - 50 > nodetop) {
+    node.$node.css('top', nodetop += 3);
+  } else if (dancertop + 50 < nodetop) {
+    node.$node.css('top', nodetop -= 3);
+  }
+};
+
+MouseDancer.prototype.checkColision = function() {
+  for (var i = 0; i < window.dancers.length; i++) {
+    var dancer = window.dancers[i];
+    this.setPos(dancer);
+  }
+};
--- src/init.js
+++ src/init.js
@@ -1,47 +1,21 @@
 $(document).ready(function() {
 
   window.dancers = [];
-
-  // $('.addColorDancer').on('click', function(e) {
-  //   var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-  //   var dancerMakerFunction = window[dancerMakerFunctionName];
-  
-  //   var dancer = new dancerMakerFunction (
-  //     $('body').height() * Math.random(),
-  //     $('body').width() * Math.random(),
-  //     Math.random() * 1000
-  //     );
-
-  //   $('body').append(dancer.$node);
-  //   window.dancers.push(dancer);
-
-  // });
-
-  // $('.addMouseDancer').on('click', function(e) {
-  //   var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-  //   var dancerMakerFunction = window[dancerMakerFunctionName];
-    
-  //   var dancer = new MouseDancer (
-  //     $('body').height() * Math.random(),
-  //     $('body').width() * Math.random(),
-  //     Math.random() * 1000
-  //     );
-
-  //   $('body').append(dancer.$node);
-  //   window.dancers.push(dancer);
-
-  //   $(document).mousemove(function(e) {
-  //     $mouseX = e.pageX;
-  //     $mouseY = e.pageY;
-  //     dancer.followMouse($mouseX, $mouseY);    
-  //   });
-
-  // });
-
+  var initializer = true;
 
   $('.addDancerButton').on('click', function(event) {
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancerMakerFunction = window[dancerMakerFunctionName];    
+
+    if (dancerMakerFunctionName === 'MouseDancer' && !initializer) {
+      var $mouseDancer = $('a:contains("mouse")');
+      $mouseDancer.addClass('not-active');
+      return;
+    }
+
+    if (dancerMakerFunctionName === 'MouseDancer') {
+      initializer = false;
+    }
 
     var dancer = new dancerMakerFunction (
       $('body').height() * Math.random(),
@@ -49,23 +23,22 @@ $(document).ready(function() {
       Math.random() * 1000
       );
 
+    $(dancer.$node).addClass('animated');
     $('body').append(dancer.$node);
+
+    if (dancerMakerFunctionName === 'MouseDancer') {
+      $(dancer.$node).addClass('MouseDancer');
+    }
     window.dancers.push(dancer);
 
     if (dancerMakerFunctionName === 'MouseDancer') {
       $(document).mousemove(function(e) {
         $mouseX = e.pageX;
         $mouseY = e.pageY;
-        dancer.followMouse($mouseX, $mouseY);    
+        dancer.followMouse($mouseX, $mouseY);
+        dancer.checkColision();    
       });
     }
-  });
-
-
-  $('.lineLeft').on('click', function(event) {
-    window.dancers.forEach(function(dancer) {
-      dancer.lineLeft();
-    });
   });
 
   $('.lineUp').on('click', function(event) {
